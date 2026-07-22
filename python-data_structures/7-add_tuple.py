da7a5e30ff0520ed3ff626cb0237396de149a107
Mohamed-code271 <a.mohamed@alustudent.com>
2026-07-22 11:43:02 +0000

    Add shebang to all files

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     a = list(tuple_a) + [0, 0]
     b = list(tuple_b) + [0, 0]
--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,6 @@
+#!
+cat > 8-multiple_returns.py << 'EOF'
+#!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
         return (0, None)
