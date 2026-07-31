d0504974a39f926bf1fdf6c055f3c2f4b05122f5
Mohamed-code271 <a.mohamed@alustudent.com>
2026-07-31 09:20:49 +0000

    Add comments to SQL_introduction scripts

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,1 +1,2 @@
+-- lists the number of records for each score, sorted by count descending
 SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,1 +1,2 @@
+-- lists records with a name value, ordered by descending score
 SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
